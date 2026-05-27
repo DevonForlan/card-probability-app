40670beae3155b58ee299088b01bd628ed20853f
Devon <q0000h6209@gmail.com>
2026-05-27 22:17:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,19 @@
-const CACHE_NAME = "card-probability-assistant-v8";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css?v=8",
-  "./app.js?v=7",
-  "./manifest.json"
-];
+const CACHE_NAME = "card-probability-assistant-v10";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   );
+
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
-  );
+  event.respondWith(fetch(event.request));
 });
